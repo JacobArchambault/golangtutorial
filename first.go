d6f1e91381a9e4e232379a7ff1d8b669aa903cc0
Jacob Archambault <jacobarchambault@gmail.com>
2026-09-09 14:09:55 -0400

    More Printf

--- first.go
+++ first.go
@@ -5,8 +5,18 @@ import "fmt"
 func main () {
 	var name string = "Arrya Paul"
 	const pi float64 = 3.14159
+	win := true
+	x := 5
+
 	fmt.Println(len(name))
 	fmt.Println(name + "is a chill dude")
 	fmt.Printf("%.3f \n",pi)
 	fmt.Printf("%T \n", name);
+	fmt.Printf("%t \n", win);
+	fmt.Printf("%d \n", x)
+	fmt.Printf("%b \n", 25)
+	fmt.Printf("%c \n", 34)
+	fmt.Printf("%x \n", 15)
+	fmt.Printf("%e \n", pi)
+
 }
