dab3e5e734c9745852d97813182c4193e224c59e
Jacob Archambault <jacobarchambault@gmail.com>
2026-09-09 14:11:11 -0400

    For loop

--- first.go
+++ first.go
@@ -3,20 +3,7 @@ package main
 import "fmt"
 
 func main () {
-	var name string = "Arrya Paul"
-	const pi float64 = 3.14159
-	win := true
-	x := 5
-
-	fmt.Println(len(name))
-	fmt.Println(name + "is a chill dude")
-	fmt.Printf("%.3f \n",pi)
-	fmt.Printf("%T \n", name);
-	fmt.Printf("%t \n", win);
-	fmt.Printf("%d \n", x)
-	fmt.Printf("%b \n", 25)
-	fmt.Printf("%c \n", 34)
-	fmt.Printf("%x \n", 15)
-	fmt.Printf("%e \n", pi)
-
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
+	}
 }
