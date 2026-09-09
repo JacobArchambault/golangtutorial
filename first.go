10541102bd8b4799c19c358b33c38d83e59677bc
Jacob Archambault <jacobarchambault@gmail.com>
2026-09-09 14:22:49 -0400

    Initialize array

--- first.go
+++ first.go
@@ -3,12 +3,7 @@ package main
 import "fmt"
 
 func main () {
-	var EvenNum[5] int
+	EvenNum := [5]int{0,2,4,6,8}
 
-	EvenNum[0] = 0
-	EvenNum[1] = 1
-	EvenNum[2] = 2
-	EvenNum[3] = 3
-	EvenNum[4] = 4
 	fmt.Println(EvenNum[2])
 }
