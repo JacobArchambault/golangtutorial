0c51342f1ae86f76d2de9f3b06adff6b86e2a44f
Jacob Archambault <jacobarchambault@gmail.com>
2026-09-15 08:42:31 -0400

    args

--- first.go
+++ first.go
@@ -3,25 +3,14 @@ package main
 import "fmt"
 
 func main () {
-	fmt.Println(div(3,0))
-	fmt.Println(div(3,5))
-	demPanic()
+	fmt.Println(addemup(10,20,30,40,50))
 }
 
-func div (num1, num2 int) int {
-	defer func() {
-		fmt.Println(recover())
-	}()
-	solution := num1/num2
-
-	return solution
-}
-
-func demPanic() {
-	defer func() {
-		fmt.Println(recover())
-	}()
-
-	panic("Panic")
+func addemup (args ...int) int {
+	sum := 0
+	for _, value := range args {
+		sum += value
+	}
+	return sum
 }
 
