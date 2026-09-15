d00a2c4c6d9267541c8dbbd0dc630a018bc12cee
Jacob Archambault <jacobarchambault@gmail.com>
2026-09-15 08:54:17 -0400

    Interfaces

--- first.go
+++ first.go
@@ -1,12 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main () {
-	rect1 := Rectangle{10,5}
-	fmt.Println(rect1.height)
-	fmt.Println(rect1.width)
-	fmt.Println("Area of rectangle is", rect1.area())
+	rect := Rectangle{50,60}
+	circ := Circle{7}
+
+	fmt.Println("Area of rectangle is", getArea(rect))
+	fmt.Println("Area of circle is", getArea(circ))
+}
+
+type Shape interface {
+	area() float64
 }
 
 type Rectangle struct {
@@ -14,7 +22,20 @@ type Rectangle struct {
 	width float64
 }
 
-func (rect *Rectangle) area() float64 {
-	return rect.height * rect.width
+type Circle struct {
+	radius float64
 }
+
+func (r1 Rectangle) area() float64 {
+	return r1.height * r1.width
+}
+
+func (c1 Circle) area() float64 {
+	return math.Pi * math.Pow(c1.radius,2)
+}
+
+func getArea(shape Shape) float64 {
+	return shape.area()
+}
+
 
