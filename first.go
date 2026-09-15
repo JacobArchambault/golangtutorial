7ad4ba3c4356e81be25a334b383fbdacc30bd666
Jacob Archambault <jacobarchambault@gmail.com>
2026-09-15 08:47:35 -0400

    Structs

--- first.go
+++ first.go
@@ -3,14 +3,18 @@ package main
 import "fmt"
 
 func main () {
-	fmt.Println(addemup(10,20,30,40,50))
+	rect1 := Rectangle{10,5}
+	fmt.Println(rect1.height)
+	fmt.Println(rect1.width)
+	fmt.Println("Area of rectangle is", rect1.area())
 }
 
-func addemup (args ...int) int {
-	sum := 0
-	for _, value := range args {
-		sum += value
-	}
-	return sum
+type Rectangle struct {
+	height float64
+	width float64
+}
+
+func (rect *Rectangle) area() float64 {
+	return rect.height * rect.width
 }
 
