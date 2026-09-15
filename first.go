65bf42a7215a755109e465739c9a526f85da4443
Jacob Archambault <jacobarchambault@gmail.com>
2026-09-15 08:16:17 -0400

    slices

--- first.go
+++ first.go
@@ -8,4 +8,15 @@ func main () {
 	for _, value := range EvenNum {
 		fmt.Println(value)
 	}
+	
+	numSlice := []int{5,4,3,2,1}
+
+	sliced := numSlice[0:]
+	fmt.Println(sliced)
+	
+	slice2 := make([]int, 5, 10)
+
+	copy(slice2, numSlice)
+
+	fmt.Println(slice2)
 }
