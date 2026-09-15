24e4167c436478cb0f0e1203afd4ce7fb2ca5745
Jacob Archambault <jacobarchambault@gmail.com>
2026-09-15 08:39:35 -0400

    Defer with panic

--- first.go
+++ first.go
@@ -3,9 +3,25 @@ package main
 import "fmt"
 
 func main () {
-	defer FirstRun()
-	SecondRun()
+	fmt.Println(div(3,0))
+	fmt.Println(div(3,5))
+	demPanic()
 }
 
-func FirstRun() { fmt.Println("I executed First")}
-func SecondRun() { fmt.Println("I executed Second")}
+func div (num1, num2 int) int {
+	defer func() {
+		fmt.Println(recover())
+	}()
+	solution := num1/num2
+
+	return solution
+}
+
+func demPanic() {
+	defer func() {
+		fmt.Println(recover())
+	}()
+
+	panic("Panic")
+}
+
